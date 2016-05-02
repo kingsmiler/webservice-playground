7cc6d9cff408661db0aa95e61bf3b012b8fc8cf7
kingsmiler <kingsmiler@163.com>
2016-05-02 10:34:41 +0800

    add jersey2, get start, update

--- jersey-rs/jersey-01-getstart/src/main/java/org/xman/ws/jersey/MyResource.java
+++ jersey-rs/jersey-01-getstart/src/main/java/org/xman/ws/jersey/MyResource.java
@@ -8,7 +8,7 @@ import javax.ws.rs.core.MediaType;
 /**
  * Root resource (exposed at "myresource" path)
  */
-@Path("myresource")
+@Path("getstart")
 public class MyResource {
 
     /**
@@ -20,6 +20,6 @@ public class MyResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String getIt() {
-        return "Got it!";
+        return "Got Start!";
     }
 }
