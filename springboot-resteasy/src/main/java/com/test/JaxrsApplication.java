0a31eee8919838d36987339b93e7b93ef64b7549
kingsmiler <kingsmiler@163.com>
2016-05-09 09:23:35 +0800

    add springboot-resteasy sample

---
+++ springboot-resteasy/src/main/java/com/test/JaxrsApplication.java
@@ -0,0 +1,13 @@
+package com.test;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * JAX-RS application
+ * <p>
+ * Created by facarvalho on 12/7/15.
+ */
+@ApplicationPath("/sample-app/")
+public class JaxrsApplication extends Application {
+}
